df2c7384948a68215a02757f7964de2acaf685ea
Vedang Sherpura <vedupatel1510@gmail.com>
2024-11-11 11:38:50 +0530

    *   ***  ***** *******

---
+++ patterns/pt/pt1.py
@@ -0,0 +1,12 @@
+#    *
+#   ***
+#  *****
+# *******
+
+
+def pyramid_pattern(n):
+    for i in range(n):
+        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+
+n = int(input("Enter the number of rows for the pyramid: "))
+pyramid_pattern(n)
